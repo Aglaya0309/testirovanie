d9ba86192a2fbb7984abe7b0a9ddb43d037667bf
ElviraKondratova <elvirochka_sh@mail.ru>
2025-07-22 16:28:14 +0300

    done

---
+++ main.py
@@ -0,0 +1,17 @@
+def add(a, b):
+   return a + b
+
+def subtract(a, b):
+   return a - b
+
+def multiply(a, b):
+   return a * b
+
+def divide(a, b):
+   return a / b
+
+
+def remainder(a, b):
+   if b == 0:
+      raise ZeroDivisionError("Деление на ноль невозможно")
+   return a % b
